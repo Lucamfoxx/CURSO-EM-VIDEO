c94dbc7b0a6473c4a1b3ca086ecb08b1bc0b4153
Lucamfoxx <guilhermedesc@outlook.com>
2022-08-04 22:45:59 -0300

    EX 56

---
+++ EXERCICIOS/MUNDO 2/EX 56.py
@@ -0,0 +1,7 @@
+sexo = str(input('Digite o sexo do bb : ')).strip().upper()[0]
+while sexo not in "MmFf":
+    sexo = str(input('INVALIDO digite o sexo do bb : ')).strip().upper()[0]
+if sexo ==("F"):
+    print('O SEXO DO BB É FEMININO')
+else :
+    print('o sexo do bb é masculino')
